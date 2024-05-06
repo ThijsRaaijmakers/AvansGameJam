5979f36ddf11752b4f8f65be78bb74b60e410ea4
Kevin van Gils <kvnvangils@gmail.com>
2024-05-06 14:15:10 +0200

    final touches

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class CursorManager : MonoBehaviour
 {
     public Texture2D cursorTexture;
+    private AudioSource audioSource;
     void Start()
     {
         Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0)) // 0 for left mouse button, 1 for right mouse button
+        {
+            // Play the sound
+            audioSource.Play();
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public float pointsPerSecond = 1f; // Points gained per second for each invisible object
+    public double pointsPerSecond = 0.1; // Points gained per second for each invisible object
     
-    private float currentPoints = 0f; // Current points earned
+    private double currentPoints = 0; // Current points earned
+    public TMP_Text text;
     
     void Update()
     {
@@ -14,12 +16,13 @@ public class GameManager : MonoBehaviour
         GameObject[] invisibleObjects = GameObject.FindGameObjectsWithTag("Hit");
         
         // Calculate points based on the number of invisible objects
-        float pointsToAdd = invisibleObjects.Length * pointsPerSecond * Time.deltaTime;
+        double pointsToAdd = invisibleObjects.Length * pointsPerSecond * Time.deltaTime;
         
         // Add points to the current total
         currentPoints += pointsToAdd;
         
         // You can do something with the points here, like displaying them on UI or using them in gameplay
         Debug.Log("Current Points: " + currentPoints);
+        text.text = currentPoints.ToString("F2");
     }
 }
