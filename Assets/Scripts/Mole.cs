f97a7e9c5fcde49e16839b06023d989a84867a1c
Kevin van Gils <kvnvangils@gmail.com>
2024-05-06 11:47:20 +0200

    Start of game

--- Assets/Scripts/Mole.cs
+++ Assets/Scripts/Mole.cs
@@ -1,18 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Mole : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Range for randomization
+    public Vector2 showDurationRange = new Vector2(1f, 3f); // Range for show duration
+    public Vector2 delayBetweenRange = new Vector2(0.5f, 1.5f); // Range for delay between hiding and showing again
+
+    private bool clicked = false;
+
+    private void Start()
     {
-        
+        // Start the coroutine
+        StartCoroutine(ShowHideObject());
     }
 
-    // Update is called once per frame
-    void Update()
+       void Update()
     {
-        
+        // Check for mouse button click
+        if (Input.GetMouseButtonDown(0)) // Change 0 to 1 if you want to detect right-clicks
+        {
+            // Get the mouse position in world coordinates
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+            // Raycast to detect if the click hits a collider
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            // If the click hits a collider
+            if (hit.collider != null)
+            {
+                // Check if the collider belongs to a sprite
+                if (hit.collider.CompareTag("Mole")) // Replace "SpriteTag" with the tag of your sprite
+                {
+                    // Disable the sprite renderer to make it disappear
+                    hit.collider.GetComponent<SpriteRenderer>().enabled = false;
+                    
+                    // Optionally, you can also destroy the GameObject
+                    // Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    IEnumerator ShowHideObject()
+    {
+        while (true)
+        {
+            // Randomize durations
+            float showDuration = Random.Range(showDurationRange.x, showDurationRange.y);
+            float delayBetween = Random.Range(delayBetweenRange.x, delayBetweenRange.y);
+
+            // Wait for random delay before showing
+            yield return new WaitForSeconds(delayBetween);
+
+            // Show the object
+            gameObject.GetComponent<Renderer>().enabled = true;
+            gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+
+            // Reset click status
+            clicked = false;
+
+            // Wait for click
+            while (!clicked)
+            {
+                yield return null;
+            }
+
+            // Reset click status
+            clicked = false;
+
+            // Delay before showing again
+            yield return null;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        clicked = true;
     }
 }
